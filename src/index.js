17f8f1c75d9674f74a7be90d91858ddb556b5b47
Simon Lee <simonhlee97@hotmail.com>
2018-04-21 16:31:06 -0400

    single page app

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
+
+
+
+// The IP Address App
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import IPAddressContainer from "./IPAddressContainer";
+ 
+// var destination = document.querySelector("#container");
+ 
+// ReactDOM.render(
+//     <div>
+//         <IPAddressContainer/>
+//     </div>,
+//     destination
+// );
